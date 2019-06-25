f862debac0fa76a22298a812fa0bfc1ad47684cb
xiangheng <xiangheng@cs.ucsb.edu>
2019-06-24 23:12:41 -0700

    n

--- h.py
+++ h.py
@@ -19,7 +19,7 @@ newImg = cv2.resize(a, (300,300))
 
 # 画图
 for i in range(len(contours)-1):
-    cv2.drawContours(newImg, contours[i+1], -1, (0,0,0), 3)
+    cv2.drawContours(newImg, contours[i+1], -1, (0,0,0), 8)
 
 cv2.imwrite('a.ppm',a)
-cv2.imwrite('i.ppm',img)
+cv2.imwrite('n.ppm',newImg)
