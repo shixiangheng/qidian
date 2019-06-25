1807e7636ae0f3e46b78b7029dac9ea4456f49e0
xiangheng <xiangheng@cs.ucsb.edu>
2019-06-24 21:20:23 -0700

    n

--- h.py
+++ h.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 img = cv2.imread("00000.ppm")    #载入图像
-mouth = img[100:446,100:815]
+mouth = img[10:446,10:815]
 h, w = img.shape[:2]      #获取图像的高和宽
 print(h)
 print(w)
