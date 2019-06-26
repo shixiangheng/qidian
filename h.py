92373190d967bbc3112691a2acc463f2340db736
shixiangheng <38025823+shixiangheng@users.noreply.github.com>
2019-06-26 13:08:11 +0800

    Update h.py

--- h.py
+++ h.py
@@ -5,12 +5,12 @@ a=img[411:446,774:815]
 img=a
 newImg = a
 newImg = cv2.resize(a, (300,300))
-
+gray = cv2.cvtColor(newImg,cv2.COLOR_RGB2GRAY)
 
 cv2.imwrite('a.ppm',a)
 cv2.imwrite('n.ppm',newImg)
 
  
-img = cv2.GaussianBlur(a,(3,3),0)
+img = cv2.GaussianBlur(gray,(3,3),0)
 e = cv2.Canny(img, 50, 150)
 cv2.imwrite('e.ppm',e)
