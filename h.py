5cf63751e8aeb221089f81248f30f9812e5cc361
shixiangheng <38025823+shixiangheng@users.noreply.github.com>
2019-06-26 12:42:23 +0800

    Update h.py

--- h.py
+++ h.py
@@ -6,20 +6,44 @@ img = cv2.imread("00000.ppm")    #载入图像
 
 a=img[411:446,774:815]
 img=a
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-ret, binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY)
-
-# 轮廓检测
-_ ,contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
 
 newImg = a
 newImg = cv2.resize(a, (300,300))
 
-# 画图
 for i in range(len(contours)-1):
     cv2.drawContours(newImg, contours[i+1], -1, (0,0,0), 8)
 
 cv2.imwrite('a.ppm',a)
 cv2.imwrite('n.ppm',newImg)
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+
+public class CannyEdgeDetection {
+   public static void main(String args[]) throws Exception {
+      // Loading the OpenCV core library
+      System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+      // Reading the Image from the file and storing it in to a Matrix object
+      String file = a;
+
+      // Reading the image
+      Mat src = Imgcodecs.imread(file);
+
+      // Creating an empty matrix to store the result
+      Mat gray = new Mat();
+
+      // Converting the image from color to Gray
+      Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
+      Mat edges = new Mat();
+
+      // Detecting the edges
+      Imgproc.Canny(gray, edges, 60, 60*3);
+
+      // Writing the image
+      Imgcodecs.imwrite("edge.ppm", edges);
+      System.out.println("Image Loaded");
+   } 
+}
