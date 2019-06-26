30b8f6aa2d5bbc5fb1c1f5fc8ace6c6146aa91cb
shixiangheng <38025823+shixiangheng@users.noreply.github.com>
2019-06-26 12:46:35 +0800

    Update h.py

--- h.py
+++ h.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("00000.ppm")    #载入图像
+img = cv2.imread("00000.ppm")   
 
 
 a=img[411:446,774:815]
@@ -15,35 +15,30 @@ for i in range(len(contours)-1):
 
 cv2.imwrite('a.ppm',a)
 cv2.imwrite('n.ppm',newImg)
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-
-import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.imgproc.Imgproc;
-
-public class CannyEdgeDetection {
-   public static void main(String args[]) throws Exception {
-      // Loading the OpenCV core library
-      System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-      // Reading the Image from the file and storing it in to a Matrix object
-      String file = a;
-
-      // Reading the image
-      Mat src = Imgcodecs.imread(file);
-
-      // Creating an empty matrix to store the result
-      Mat gray = new Mat();
-
-      // Converting the image from color to Gray
-      Imgproc.cvtColor(src, gray, Imgproc.COLOR_BGR2GRAY);
-      Mat edges = new Mat();
-
-      // Detecting the edges
-      Imgproc.Canny(gray, edges, 60, 60*3);
-
-      // Writing the image
-      Imgcodecs.imwrite("edge.ppm", edges);
-      System.out.println("Image Loaded");
-   } 
-}
+from __future__ import print_function
+import cv2 as cv
+import argparse
+max_lowThreshold = 100
+window_name = 'Edge Map'
+title_trackbar = 'Min Threshold:'
+ratio = 3
+kernel_size = 3
+def CannyThreshold(val):
+    low_threshold = val
+    img_blur = cv.blur(src_gray, (3,3))
+    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
+    mask = detected_edges != 0
+    dst = src * (mask[:,:,None].astype(src.dtype))
+    cv.imshow(window_name, dst)
+parser = argparse.ArgumentParser(description='Code for Canny Edge Detector tutorial.')
+parser.add_argument('--input', help='Path to input image.', default=a)
+args = parser.parse_args()
+src = cv.imread(cv.samples.findFile(args.input))
+if src is None:
+    print('Could not open or find the image: ', args.input)
+    exit(0)
+src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+cv.namedWindow(window_name)
+cv.createTrackbar(title_trackbar, window_name , 0, max_lowThreshold, CannyThreshold)
+CannyThreshold(0)
+cv.waitKey()
