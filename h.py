389b31f723c1772f1686c611707a80741bd43b33
shixiangheng <38025823+shixiangheng@users.noreply.github.com>
2019-06-26 12:52:38 +0800

    Update h.py

--- h.py
+++ h.py
@@ -1,14 +1,13 @@
-from __future__ import print_function
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-import cv2 as cv
-import argparse
 img = cv2.imread("00000.ppm")   
 
 
 a=img[411:446,774:815]
 img=a
+e = cv2.Canny(a,100,200)
 
 newImg = a
 newImg = cv2.resize(a, (300,300))
@@ -16,28 +15,5 @@ newImg = cv2.resize(a, (300,300))
 
 cv2.imwrite('a.ppm',a)
 cv2.imwrite('n.ppm',newImg)
+cv2.imwrite('e.ppm',e)
 
-max_lowThreshold = 100
-window_name = 'Edge Map'
-title_trackbar = 'Min Threshold:'
-ratio = 3
-kernel_size = 3
-def CannyThreshold(val):
-    low_threshold = val
-    img_blur = cv.blur(src_gray, (3,3))
-    detected_edges = cv.Canny(img_blur, low_threshold, low_threshold*ratio, kernel_size)
-    mask = detected_edges != 0
-    dst = src * (mask[:,:,None].astype(src.dtype))
-    cv.imshow(window_name, dst)
-parser = argparse.ArgumentParser(description='Code for Canny Edge Detector tutorial.')
-parser.add_argument('--input', help='Path to input image.', default=a)
-args = parser.parse_args()
-src = cv.imread(cv.samples.findFile(args.input))
-if src is None:
-    print('Could not open or find the image: ', args.input)
-    exit(0)
-src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-cv.namedWindow(window_name)
-cv.createTrackbar(title_trackbar, window_name , 0, max_lowThreshold, CannyThreshold)
-CannyThreshold(0)
-cv.waitKey()
