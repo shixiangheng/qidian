27b8cb22fe1e0d85d069c3912a4dc1e95b6972f2
shixiangheng <38025823+shixiangheng@users.noreply.github.com>
2019-06-26 13:16:23 +0800

    Update h.py

--- h.py
+++ h.py
@@ -4,14 +4,12 @@ img = cv2.imread("00000.ppm")
 a=img[411:446,774:815]
 img=a
 newImg = a
-newImg = cv2.resize(a, (500,500))
-gray = cv2.cvtColor(newImg,cv2.COLOR_RGB2GRAY)
-
+newImg = cv2.resize(a, (300,300))
 cv2.imwrite('a.ppm',a)
 cv2.imwrite('n.ppm',newImg)
 
- 
+gray = cv2.cvtColor(newImg,cv2.COLOR_RGB2GRAY)
 img = cv2.GaussianBlur(gray,(3,3),0)
-img = cv2.Canny(img, 50, 150)
-img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+#img = cv2.Canny(img, 50, 150)
+#img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 cv2.imwrite('e.ppm',img)
