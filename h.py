8eff54e2e92f3e009983346a8b657d61fa802405
shixiangheng <38025823+shixiangheng@users.noreply.github.com>
2019-06-26 13:01:41 +0800

    Update h.py

--- h.py
+++ h.py
@@ -1,19 +1,16 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-
 img = cv2.imread("00000.ppm")   
-
-
 a=img[411:446,774:815]
 img=a
-e = cv2.Canny(a,100,200)
-
 newImg = a
 newImg = cv2.resize(a, (300,300))
 
 
 cv2.imwrite('a.ppm',a)
 cv2.imwrite('n.ppm',newImg)
-cv2.imwrite('e.ppm',e)
 
+ 
+img = cv2.GaussianBlur(a,(3,3),0)
+e = cv2.Canny(img, 50, 150)
+cv2.imwrite('e.ppm',e)
