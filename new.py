c7143e915a95b93f3b24474bfa49362b58265e99
shixiangheng <38025823+shixiangheng@users.noreply.github.com>
2019-07-09 11:07:12 +0800

    Update new.py

--- new.py
+++ new.py
@@ -7,10 +7,9 @@ val = raw_input("Enter your value: ")
 print(type(val)) 
 
 img=cv2.imread(val)
-
-
-cv2.Canny(img, 200, 300)
-#n = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+img = cv2.GaussianBlur(gray,(3,3),0)
+canny = cv2.Canny(img, 50, 150)
+img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 cv2.imwrite("n.jpg", img)
-
 #print(s)
