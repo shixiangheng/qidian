6244290f432e85b81feb4d1f56469b19695ef4c0
shixiangheng <38025823+shixiangheng@users.noreply.github.com>
2019-07-09 12:10:45 +0800

    Update new.py

--- new.py
+++ new.py
@@ -5,7 +5,9 @@ import os
 
 img = cv2.imread("spl.jpg")
 #img=cv2.Canny(img, 200, 300)
-
+imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,thresh = cv2.threshold(imgray,127,255,0)
+im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img, contours, -1, (0,255,0), 3)
 cv2.imwrite("c.jpg", img)
 #cv2.imshow("canny", cv2.imread("canny.jpg"))
