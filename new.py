4bdda1c9ac026b8fabe4caab35fe4f6811e95409
xiangheng <xiangheng@cs.ucsb.edu>
2019-07-14 11:36:59 -0700

    n

--- new.py
+++ new.py
@@ -7,8 +7,10 @@ img = cv2.imread("s.png")
 #img=cv2.Canny(img, 200, 300)
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,127,255,cv2.THRESH_BINARY)
-im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-cv2.drawContours(img, contours, -1, (0,0,255), 3)
+th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                                       cv2.THRESH_BINARY,11,2)
+im2, contours, hierarchy = cv2.findContours(th3,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(img, contours, -1, (0,0,0), 5)
 cv2.imwrite("c.png", img)
 #cv2.imshow("canny", cv2.imread("canny.jpg"))
 '''
