893ab7ca00aac8501e48fd30852702fa1bd9a678
xiangheng <xiangheng@cs.ucsb.edu>
2019-07-14 11:17:42 -0700

    n

--- new.py
+++ new.py
@@ -9,7 +9,7 @@ imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret,thresh = cv2.threshold(imgray,127,255,0)
 im2, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img, contours, -1, (0,0,255), 3)
-cv2.imwrite("abc.png", img)
+cv2.imwrite("c.png", img)
 #cv2.imshow("canny", cv2.imread("canny.jpg"))
 '''
 img= cv2.imread("canny.jpg")
